9b7300969ea114c94b6336fca3f6bae0a83d254b
MEHDIHABIB <78062259+MEHDIHABIB@users.noreply.github.com>
2022-02-08 17:13:55 +0000

    Generate constructors

--- src/main/java/com/qa/rentalcar/domain/Rental.java
+++ src/main/java/com/qa/rentalcar/domain/Rental.java
@@ -52,6 +52,43 @@ import javax.validation.constraints.NotNull;
 		private float rentalCost;
 
 	
+		// Default constructor
+		public Rental() {
+			super();
+		}
+
+
+		
+		//Constructor without id for customer view
+		public Rental(int clienId, int carId, String clientName, String carType, String carBrand, int rentalPeriod,
+				boolean carBack, float rentalCost) {
+			super();
+			this.clienId = clienId;
+			this.carId = carId;
+			this.clientName = clientName;
+			this.carType = carType;
+			this.carBrand = carBrand;
+			this.rentalPeriod = rentalPeriod;
+			this.carBack = carBack;
+			this.rentalCost = rentalCost;
+		}
+
+
+	    //Constructor with id for admin view
+		public Rental(int id, int clienId, int carId, String clientName, String carType, String carBrand, int rentalPeriod,
+				boolean carBack, float rentalCost) {
+			super();
+			this.id = id;
+			this.clienId = clienId;
+			this.carId = carId;
+			this.clientName = clientName;
+			this.carType = carType;
+			this.carBrand = carBrand;
+			this.rentalPeriod = rentalPeriod;
+			this.carBack = carBack;
+			this.rentalCost = rentalCost;
+		}
+
 	
 	
 	
